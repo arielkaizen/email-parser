8b46a81e90158fe706d9014e9ebbd47f80448ad4
Kevin Santamaria <sr.kevin.san@gmail.com>
2025-02-26 23:53:54 -0600

    Fix filter and parser for attatchment function

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -7,16 +7,14 @@ export class EmailService {
 
     async parseEmail(emailPath: string) {
         const email = fs.readFileSync(emailPath, 'utf8');
-        const parsedEmail:ParsedMail = await simpleParser(email);
+        const parsedEmail: ParsedMail = await simpleParser(email);
         return this.getJsonAttatchments(parsedEmail);
     }
 
     async getJsonAttatchments(parsedEmail: ParsedMail) {
         const attachments = parsedEmail.attachments;
-        return attachments.map(attachment => {
-            if (attachment.contentType === 'application/json') {
-                return JSON.parse(attachment.content.toString());
-            }
+        return attachments.filter(attachment => attachment.contentType === 'application/json').map(attachment => {
+            return JSON.parse(attachment.content.toString());
         });
     }
     
