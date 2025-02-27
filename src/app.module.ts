66ea3ded9c0b3bde3d4d9469dd0bec96f56d3155
Kevin Santamaria <sr.kevin.san@gmail.com>
2025-02-26 23:22:45 -0600

    add project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailController } from './email/email.controller';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [EmailModule],
+  controllers: [AppController, EmailController],
   providers: [AppService],
 })
 export class AppModule {}
